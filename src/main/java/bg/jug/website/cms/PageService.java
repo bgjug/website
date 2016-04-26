15a7c69630ab31e3b1715e1c8314b62f7e9020a9
ivannov <ivan_st_ivanov@yahoo.com>
2016-04-26 22:24:15 +0300

    Added creating a page

---
+++ src/main/java/bg/jug/website/cms/PageService.java
@@ -0,0 +1,30 @@
+package bg.jug.website.cms;
+
+import bg.jug.website.model.cms.Page;
+import bg.jug.website.repository.cms.PageRepository;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Ivan St. Ivanov
+ */
+@Stateless
+@Path("/page")
+public class PageService {
+
+    @Inject
+    private PageRepository pageRepository;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createPage(Page newPage) {
+        Page createdPage = pageRepository.save(newPage);
+        return Response.status(Response.Status.CREATED).entity(createdPage).build();
+    }
+}
---
+++ src/main/java/bg/jug/website/config/RestApplication.java
@@ -0,0 +1,11 @@
+package bg.jug.website.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Ivan St. Ivanov
+ */
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
